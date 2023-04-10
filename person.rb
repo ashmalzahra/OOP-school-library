7d3409644beb50f67e927039498a7a950517ebe6
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-10 21:46:47 +0500

    Create Teacher class

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
-    def initialize(name = "Unknown", age, parent_permission = true)
-        @id = id
+    def initialize(name = "Unknown", age, parent_permission: true)
+        @id = Random.rand(1..1000)
         @name = name
         @age = age
         @parent_permission = parent_permission
@@ -19,7 +19,7 @@ class Person
     end
 
     def can_use_services?
-        if @age >= 18? true || parent_permission? true : false
+        if is_of_age? true || parent_permission? true : false
     end
 
     private :is_of_age?
--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
+require './person.rb'
+
 class Student < Person
-    def initialize(classroom)
-        super()
+    def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+        super(name, age, parent_permission)
         @classroom = classroom
     end
 
