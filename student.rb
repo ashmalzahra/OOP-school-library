33716e72de0bd9ff7b1857004cc6fad77d44c851
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-10 22:45:25 +0500

    Configure Linters

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require './person.rb'
 
 class Student < Person
-    def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-        super(name, age, parent_permission)
-        @classroom = classroom
-    end
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 require './person.rb'
 
 class Teacher < Person
-    def initialize(specialization, name = "Unknown", parent_permission: true)
-        super(name, age, parent_permission)
-        @specialization = specialization
-    end
+  def initialize(specialization, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
