359450548038679494fda832325595d27e14734e
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-12 21:37:37 +0500

    Create the has-many/belongs-to relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -9,4 +9,11 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  attr_reader :classroom
 end
