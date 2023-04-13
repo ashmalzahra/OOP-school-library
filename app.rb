c0e018dbc335e39dad66739108b8925ba31795d1
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-13 22:26:41 +0500

    Create Rental

--- app.rb
+++ app.rb
@@ -76,6 +76,28 @@ class  App
         puts 'Book added successfully'
       end
     end
+
+    def create_rental
+        puts 'Select a book from the following list by number'
+        @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+      
+        book_id = gets.chomp.to_i
+      
+        puts 'Select a person from the following list by number (not id)'
+        @people.each_with_index do |person, index|
+          puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        end
+      
+        person_id = gets.chomp.to_i
+      
+        print 'Date: '
+        date = gets.chomp.to_s
+      
+        rental = Rental.new(date, @people[person_id], @books[book_id])
+        @rentals << rental
+      
+        puts 'Rental created successfully'
+    end
 end
 
 
