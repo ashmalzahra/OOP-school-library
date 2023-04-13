098f1c8fa6e452d1558ff174a7cb50e305777c30
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-13 23:19:07 +0500

    Modify creat_person to reduce complexcity

--- app.rb
+++ app.rb
@@ -5,10 +5,12 @@ require './teacher'
 require './rental'
 
 class App
+  attr_accessor :people, :books, :rentals
+
   def initialize
-    @people = people
-    @books = books
-    @rental = rental
+    @people = []
+    @books = []
+    @rentals = []
   end
 
   def list_books
@@ -18,7 +20,7 @@ class App
 
   def list_people
     puts "No person registered yet" if @people.empty?
-    @people.map { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    @people.map { |person| puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
   end
 
   def create_person
@@ -26,41 +28,49 @@ class App
     person = gets.chomp.to_i
 
     case person
-
     when 1
-      print 'Age: '
-      age = gets.chomp.to_i
-      print 'Name: '
-      name = gets.chomp
-      print 'Has parent permissiom [Y/N]? '
-      permission = gets.chomp.downcase
-
-      case permission
-      when 'y'
-        parent_permission = true
-      when 'n'
-        parent_permission = false
-      else
-        puts "Invalid input. Please enter 'Y' or 'N'"
-      end
-
-      student = Student.new(age, name, parent_permission: parent_permission)
-      @people << student
-
-      puts 'Student created successfully'
-
+      create_student
     when 2
-      print 'Age: '
-      age = gets.chomp.to_i
-      print 'Name: '
-      name = gets.chomp
-      print 'Specialization: '
-      specialization = gets.chomp
-      teacher = Teacher.new(age, name, specialization)
-      @people << teacher
-
-      puts 'Teacher created successfully'
+      create_teacher
+    else
+      puts 'Invalid input. Kindly type 1 or 2'
     end
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission [Y/N]? '
+    permission = gets.chomp.downcase
+
+    case permission
+    when 'y'
+      parent_permission = true
+    when 'n'
+      parent_permission = false
+    else
+      puts "Invalid input. Please enter 'Y' or 'N'"
+    end
+
+    student = Student.new(age, name, parent_permission: parent_permission)
+    @people << student
+
+    puts 'Student created successfully'
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, name, specialization)
+    @people << teacher
+
+    puts 'Teacher created successfully'
   end
 
   def create_book
@@ -75,36 +85,36 @@ class App
 
     puts 'Book added successfully'
   end
-end
 
-def create_rental
-  puts 'Select a book from the following list by number'
-  @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
 
-  book_id = gets.chomp.to_i
+    book_id = gets.chomp.to_i
 
-  puts 'Select a person from the following list by number (not id)'
-  @people.each_with_index do |person, index|
-    puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+
+    person_id = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_s
+
+    rental = Rental.new(date, @people[person_id], @books[book_id])
+    @rentals << rental
+
+    puts 'Rental created successfully'
   end
 
-  person_id = gets.chomp.to_i
+  def list_rentals
+    print 'ID of person: '
+    id = gets.chomp.to_i
 
-  print 'Date: '
-  date = gets.chomp.to_s
-
-  rental = Rental.new(date, @people[person_id], @books[book_id])
-  @rentals << rental
-
-  puts 'Rental created successfully'
-end
-
-def list_rentals
-  print 'ID of person: '
-  id = gets.chomp.to_i
-
-  puts 'Rentals:'
-  @rentals.each do |rental|
-    puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    puts 'Rentals:'
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    end
   end
 end
