b7a398926cb8f195a6a91f70a497d1ee5a806c5a
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-20 21:35:21 +0500

    Create test for Decorator classes

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -11,13 +11,13 @@ describe Teacher do
     end
 
     it "should initialize specialization as 'Data Science'" do
-        expect(@teacher.instance_variable_get(:@specialization)).to eq 'Data Science'
-      end
+      expect(@teacher.instance_variable_get(:@specialization)).to eq 'Data Science'
+    end
   end
 
   describe '#can_use_services?' do
     it 'should always return true' do
-        expect(@teacher.can_use_services?).to be_truthy
+      expect(@teacher.can_use_services?).to be_truthy
     end
   end
 end
